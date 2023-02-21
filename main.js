d43949d1b283a525270fb5e6612f0fc1927bb3a5
Ale <97320635+marilesmartineza@users.noreply.github.com>
2023-02-20 19:14:37 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,151 @@
+prediction_1 = ""
+prediction_2 = ""
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90 
+  });
+
+camera = document.getElementById("camera");
+ 
+Webcam.attach('#camera');
+
+      
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+  console.log('ml5 version:', ml5.version);
+  
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/7ZQTqzSHy/model.json',modelLoaded);
+
+  function modelLoaded() {
+    console.log('Model Loaded!');
+  }
+  
+function speak(){
+  var synth = window.speechSynthesis;
+  speak_data_1 = "La primera prediccion es " + prediction_1;
+  speak_data_2 = "Y la segunda prediccion es " + prediction_2;
+  var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+  synth.speak(utterThis);
+}
+
+
+  function check()
+  {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+  }
+
+
+function gotResult(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+    document.getElementById("result_emotion_name").innerHTML = results[0].label;
+    document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+    prediction_1 = results[0].label;
+    prediction_2 = results[1].label;
+    speak();
+    if(results[0].label == "Hola")
+    {
+	    document.getElementById("update_emoji").innerHTML = "&#128075;";
+    }
+    //if(results[0].label == "Like")
+    {
+	    document.getElementById("update_emoji").innerHTML = "&#128077;";
+    }
+    if(results[0].label == "Dislike")
+    {
+	    document.getElementById("update_emoji").innerHTML = "&#128078;";
+    }
+    if(results[0].label == "Give me five")
+    {
+	    document.getElementById("update_emoji").innerHTML = "&#128079;";
+    }
+    if(results[0].label == "Pistolita")
+    {
+	    document.getElementById("update_emoji").innerHTML = "&#128299	;";
+    }
+    if(results[0].label == "Corazón")
+    {
+	    document.getElementById("update_emoji").innerHTML = "&#128147;";
+    }
+    if(results[0].label == "Rock")
+    {
+	    document.getElementById("update_emoji").innerHTML = "&#129311;";
+    }
+    if(results[0].label == "Groser@")
+    {
+	    document.getElementById("update_emoji").innerHTML = "&#129324;";
+    }
+    if(results[0].label == "Money")
+    {
+	    document.getElementById("update_emoji").innerHTML = "&#129297;";
+    }
+    if(results[0].label == "Alien")
+    {
+	    document.getElementById("update_emoji").innerHTML = "&#128406;";
+    }
+    if(results[0].label == "Ok")
+    {
+	    document.getElementById("update_emoji").innerHTML = "&#128076;";
+    }
+
+
+
+    if(results[1].label == "Hola")
+    {
+	    document.getElementById("update_emoji").innerHTML = "&#128075;";
+    }
+    //if(results[1].label == "Like")
+    {
+	    document.getElementById("update_emoji").innerHTML = "&#128077;";
+    }
+    if(results[1].label == "Dislike")
+    {
+	    document.getElementById("update_emoji").innerHTML = "&#128078;";
+    }
+    if(results[1].label == "Give me five")
+    {
+	    document.getElementById("update_emoji").innerHTML = "&#128079	;";
+    }
+    if(results[1].label == "Pistolita")
+    {
+	    document.getElementById("update_emoji").innerHTML = "&#128299	;";
+    }
+    if(results[1].label == "Corazón")
+    {
+	    document.getElementById("update_emoji").innerHTML = "&#128147;";
+    }
+    if(results[1].label == "Rock")
+    {
+	    document.getElementById("update_emoji").innerHTML = "&#129311;";
+    }
+    if(results[1].label == "Groser@")
+    {
+	    document.getElementById("update_emoji").innerHTML = "&#129324;";
+    }
+    if(results[1].label == "Money")
+    {
+	    document.getElementById("update_emoji").innerHTML = "&#129297;";
+    }
+    if(results[1].label == "Alien")
+    {
+	    document.getElementById("update_emoji").innerHTML = "&#128406;";
+    }
+    if(results[1].label == "Ok")
+    {
+	    document.getElementById("update_emoji").innerHTML = "&#128076;";
+    }
+
+  }
+}
+
